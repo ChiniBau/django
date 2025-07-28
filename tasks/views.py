9c05e21f0504a0d3831909687d150c0df6f12093
ChiniBau <lordyunik@gmail.com>
2025-07-28 18:34:08 +0545

    task-list updated

--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
-from .forms import TaskForm, RegistrationForm
+from .forms import TaskForm, RegisterForm
 from django.contrib.auth import login, logout
 
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return render(request, 'tasks/task_list.html')
+            return redirect('task-list')
     else:
-        form = RegistrationForm()
+        form = RegisterForm()
     return render(request, 'tasks/register.html', {'form': form})
 
 @login_required
 def task_list(request):
     tasks = Task.objects.filter(user=request.user)
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})  
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 @login_required
 def task_create(request):
@@ -28,7 +29,7 @@ def task_create(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
-            return render(request, 'tasks/task_list.html')
+            return redirect('task-list')
     else:
         form = TaskForm()
-    return render(request, 'tasks/task_form.html', {'form': form})
+    return render(request, 'tasks/tasks_form.html', {'form': form})
--- yu1/urls.py
+++ yu1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for yu1 project.
+URL configuration for django1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
